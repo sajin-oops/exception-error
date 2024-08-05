74f5eaa091360849570c3f58f4008b873da49996
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-05 23:09:38 +0530

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,7 @@
+#Exceptions are raised when the program is syntactically correct but the code results in a error. 
+
+marks = 10000
+a = marks / 0
+print(a)
+
+#O/P - ZeroDivisionError: division by zero
